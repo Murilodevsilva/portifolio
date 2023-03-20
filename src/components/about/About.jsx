dc58234bcfc865cd53dd4a081bb829bf245e65fb
portfolio <murilodev.silva@gmail.com>
2023-03-20 08:11:28 -0300

    portfolio alteração

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,11 +11,6 @@ const About = () => {
       <h2>Programação</h2>
 
       <div className='container about__container'>
-        <div className='about__me'>
-          <div className='about__me-image'>
-            <img src={ME} alt="imagem sobre" />
-          </div>
-        </div>
           <div className='about__content'>
             <div className='about__cards'>
               <article className='about__card'>
